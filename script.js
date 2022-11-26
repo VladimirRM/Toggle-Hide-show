fd53219f03c3e2887788427f1fb0dc27bfeb0f41
Vladimir RM <shvdrummer@gmail.com>
2022-11-26 09:14:16 +0200

    create alert

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function myFunction(){
-    
+function myFunction() {
+  const x = document.getElementById("myDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
